01b254bccecb9b9db8acb0d7718b2120e837a812
chamika-damith <chamikadamith9@gmail.com>
2024-02-29 14:53:43 +0530

    user dashboard

--- src/main/java/org/example/controller/user/UserDashboardController.java
+++ src/main/java/org/example/controller/user/UserDashboardController.java
@@ -1,4 +1,27 @@
 package org.example.controller.user;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class UserDashboardController {
+    public AnchorPane changeRoot;
+
+    public void btnBookOnAction(ActionEvent actionEvent) throws IOException {
+        Parent parent= FXMLLoader.load(getClass().getResource("/view/user/bookPages/AllBookForm.fxml"));
+        changeRoot.getChildren().clear();
+        changeRoot.getChildren().add(parent);
+    }
+
+    public void btnBorrowBooksOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnTransactionOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btndashboardOnAction(ActionEvent actionEvent) {
+    }
 }
