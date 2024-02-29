5ab737110cf46568ec44e76352b9a3c91e94338b
chamika-damith <chamikadamith9@gmail.com>
2024-02-29 14:53:21 +0530

    user login action

--- src/main/java/org/example/controller/user/loginPages/UserLoginFormController.java
+++ src/main/java/org/example/controller/user/loginPages/UserLoginFormController.java
@@ -1,9 +1,12 @@
 package org.example.controller.user.loginPages;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -14,5 +17,14 @@ public class UserLoginFormController {
         Parent parent= FXMLLoader.load(getClass().getResource("/view/user/loginPages/UserRegisterForm.fxml"));
         userLoginRoot.getChildren().clear();
         userLoginRoot.getChildren().add(parent);
+    }
+
+    public void btnUserLoginOnAction(ActionEvent actionEvent) throws IOException {
+        Parent parent= FXMLLoader.load(getClass().getResource("/view/user/UserDashboard.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
     }
 }
