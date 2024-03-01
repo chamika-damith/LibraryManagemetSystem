e8d6d10fb5e3f95155b5bbcb7e8a5cf3ecc2baa3
chamika-damith <chamikadamith9@gmail.com>
2024-03-01 22:08:12 +0530

    book bo

--- src/main/java/org/example/bo/custom/BookBO.java
+++ src/main/java/org/example/bo/custom/BookBO.java
@@ -3,6 +3,9 @@ package org.example.bo.custom;
 import org.example.bo.SuperBO;
 import org.example.dto.BookDto;
 
+import java.util.List;
+
 public interface BookBO extends SuperBO {
     boolean addBook(BookDto dto);
+    List<BookDto> getAllBooks();
 }
