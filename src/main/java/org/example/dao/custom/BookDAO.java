5d5c32d1b2c23e481a39d5bd4af1f9f7eb8f8bbd
chamika-damith <chamikadamith9@gmail.com>
2024-03-01 14:37:40 +0530

    book DAO Classes

---
+++ src/main/java/org/example/dao/custom/BookDAO.java
@@ -0,0 +1,8 @@
+package org.example.dao.custom;
+
+import org.example.dao.CrudDAO;
+import org.example.dao.SuperDAO;
+import org.example.entity.Book;
+
+public interface BookDAO extends CrudDAO<Book> {
+}
---
+++ src/main/java/org/example/dao/custom/impl/BookDAOImpl.java
@@ -0,0 +1,11 @@
+package org.example.dao.custom.impl;
+
+import org.example.dao.custom.BookDAO;
+import org.example.entity.Book;
+
+public class BookDAOImpl implements BookDAO {
+    @Override
+    public boolean addBook(Book dto) {
+        return false;
+    }
+}
