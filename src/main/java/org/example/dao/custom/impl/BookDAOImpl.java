bc8d3cbc011fc81e5cd05e4fbefe025b699b26fa
chamika-damith <chamikadamith9@gmail.com>
2024-03-01 22:09:04 +0530

    book dao impl

--- src/main/java/org/example/dao/custom/impl/BookDAOImpl.java
+++ src/main/java/org/example/dao/custom/impl/BookDAOImpl.java
@@ -1,11 +1,37 @@
 package org.example.dao.custom.impl;
 
+import org.example.config.FactoryConfiguration;
 import org.example.dao.custom.BookDAO;
 import org.example.entity.Book;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class BookDAOImpl implements BookDAO {
     @Override
-    public boolean addBook(Book dto) {
-        return false;
+    public boolean add(Book dto) {
+        Session session= FactoryConfiguration.getInstance().getSession();
+        Transaction transaction=session.beginTransaction();
+
+        session.save(dto);
+
+        transaction.commit();
+        session.close();
+        return true;
+    }
+
+    @Override
+    public List<Book> getAll() {
+        Session session= FactoryConfiguration.getInstance().getSession();
+        Transaction transaction=session.beginTransaction();
+
+        Query<Book> query = session.createQuery("FROM Book", Book.class);
+        List<Book> resultList = query.getResultList();
+
+        transaction.commit();
+        session.close();
+        return resultList;
     }
 }
