025ce648fc51d4f066428a12888df2de10e99e67
chamika-damith <chamikadamith9@gmail.com>
2024-03-01 22:10:37 +0530

    updated hibernate FactoryConfiguration

--- src/main/java/org/example/config/FactoryConfiguration.java
+++ src/main/java/org/example/config/FactoryConfiguration.java
@@ -1,5 +1,29 @@
 package org.example.config;
 
-public class FactoryConfiguration {
+import org.example.entity.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+public class FactoryConfiguration {
+    public static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
+        Configuration configuration=new Configuration().configure().addAnnotatedClass(Book.class);
+        sessionFactory=configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance(){
+        if (factoryConfiguration==null){
+            factoryConfiguration=new FactoryConfiguration();
+        }else {
+            return factoryConfiguration;
+        }
+        return factoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
 }
