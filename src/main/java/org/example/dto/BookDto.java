5b290275cffc0a71404060ca9956b96de02d879b
chamika-damith <chamikadamith9@gmail.com>
2024-03-01 14:35:57 +0530

    book dto

---
+++ src/main/java/org/example/dto/BookDto.java
@@ -0,0 +1,16 @@
+package org.example.dto;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class BookDto {
+    private String bookId;
+    private String title;
+    private String author;
+    private String genre;
+    private boolean availability;
+}
