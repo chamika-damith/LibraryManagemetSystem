813a046852ac3ccf91a0cb9b84898e7185093932
chamika-damith <chamikadamith9@gmail.com>
2024-03-01 22:08:37 +0530

    book bo impl

--- src/main/java/org/example/bo/custom/impl/BookBOImpl.java
+++ src/main/java/org/example/bo/custom/impl/BookBOImpl.java
@@ -6,13 +6,29 @@ import org.example.dao.custom.BookDAO;
 import org.example.dto.BookDto;
 import org.example.entity.Book;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookBOImpl implements BookBO {
 
     BookDAO bookDAO= (BookDAO) DAOFactory.getDaoFactory().getDao(DAOFactory.DAOType.BOOK);
 
     @Override
     public boolean addBook(BookDto dto) {
-        return bookDAO.addBook(new Book(dto.getBookId(),dto.getTitle(),dto.getAuthor(),dto.getGenre(),
-                dto.isAvailability()));
+        return bookDAO.add(new Book(dto.getBookId(),dto.getTitle(),dto.getAuthor(),dto.getGenre(),
+                dto.getAvailability()));
+    }
+
+    @Override
+    public List<BookDto> getAllBooks() {
+        List<Book> all = bookDAO.getAll();
+
+        List<BookDto> allBooks = new ArrayList<>();
+
+        for (Book book : all) {
+            allBooks.add(new BookDto(book.getBookId(),book.getTitle(),book.getAuthor(),book.getGenre(),
+            book.getAvailability()));
+        }
+        return allBooks;
     }
 }
