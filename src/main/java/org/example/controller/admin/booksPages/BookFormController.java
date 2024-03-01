d5e96ea1dcec9e01c21fa84ea585401442419aca
chamika-damith <chamikadamith9@gmail.com>
2024-03-01 14:35:02 +0530

    add book function

--- src/main/java/org/example/controller/admin/booksPages/BookFormController.java
+++ src/main/java/org/example/controller/admin/booksPages/BookFormController.java
@@ -1,11 +1,67 @@
 package org.example.controller.admin.booksPages;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
+import javafx.scene.paint.Color;
 
 public class BookFormController {
     public TableView bookTable;
+    public JFXTextField bookTitle;
+    public JFXTextField bookAuthor;
+    public JFXTextField bookGenre;
+    public JFXComboBox availabilityStatus;
+    public JFXTextField bookId;
 
     public void initialize(){
         bookTable.getStylesheets().add("/style/style.css");
+    }
+
+    public void btnAddBookOnAction(ActionEvent actionEvent) {
+        if(isEmptyCheck()){
+            Image image=new Image("assest/login image/icon/icons8-cancel-50.png");
+            try {
+                Notifications notifications=Notifications.create();
+                notifications.graphic(new ImageView(image));
+                notifications.text("Value is empty! Please enter all values");
+                notifications.title("Warning");
+                notifications.hideAfter(Duration.seconds(5));
+                notifications.position(Pos.TOP_RIGHT);
+                notifications.show();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }else {
+            //add book method
+        }
+    }
+
+    public boolean isEmptyCheck() {
+
+        if(bookTitle.getText().isEmpty()){
+            bookTitle.requestFocus();
+            bookTitle.setFocusColor(Color.RED);
+            System.out.println("Book title field is empty");
+            return true;
+        }
+        if(bookAuthor.getText().isEmpty()){
+            bookAuthor.requestFocus();
+            bookAuthor.setFocusColor(Color.RED);
+            System.out.println("Book author field is empty");
+            return true;
+        }
+        if (bookGenre.getText().isEmpty()){
+            bookGenre.requestFocus();
+            bookGenre.setFocusColor(Color.RED);
+            System.out.println("Book genre field is empty");
+            return true;
+        }
+        return false;
     }
 }
