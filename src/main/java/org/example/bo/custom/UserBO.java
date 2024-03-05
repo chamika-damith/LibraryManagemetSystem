98e8ee820cbb8028ad9be1e317fb12f36e9fe4f8
chamika-damith <chamikadamith9@gmail.com>
2024-03-05 12:11:54 +0530

    create user Bo

---
+++ src/main/java/org/example/bo/custom/UserBO.java
@@ -0,0 +1,16 @@
+package org.example.bo.custom;
+
+import org.example.dto.BookDto;
+import org.example.dto.UserDto;
+import org.example.entity.User;
+
+import java.util.List;
+
+public interface UserBO {
+    boolean addUser(UserDto dto);
+    List<UserDto> getAllUser();
+    boolean updateUser(UserDto dto);
+    boolean isExistUser(String id);
+    BookDto searchUser(String id);
+    boolean deleteUser(String id);
+}
