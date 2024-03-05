ad1f4cb29f36ae22f6e8cda36fe82b874ce44362
chamika-damith <chamikadamith9@gmail.com>
2024-03-05 21:14:38 +0530

    update BranchFormController

--- src/main/java/org/example/controller/admin/branchPages/BranchFormController.java
+++ src/main/java/org/example/controller/admin/branchPages/BranchFormController.java
@@ -1,4 +1,28 @@
 package org.example.controller.admin.branchPages;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class BranchFormController {
+    public JFXTextField txtBranchId;
+    public JFXTextField txtBranchName;
+    public JFXTextField txtBranchLocation;
+    public TableView branchTable;
+    public TableColumn colBranchId;
+    public TableColumn colBranchName;
+    public TableColumn colBranchLocation;
+    public TableColumn colCloseBranch;
+    public JFXTextField txtSearchBar;
+
+    public void btnAddBranchOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnUpdateBranchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchBranchOnAction(ActionEvent actionEvent) {
+    }
 }
